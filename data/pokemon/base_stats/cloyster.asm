291c10fd43752d72e6f7058f1f8a01208b4330fe
clodsire74 <davidstormking0@gmail.com>
2025-10-15 14:44:29 -0700

    Update cloyster.asm

--- data/pokemon/base_stats/cloyster.asm
+++ data/pokemon/base_stats/cloyster.asm
@@ -3,22 +3,21 @@
 	db  50,  95, 180,  70,  85
 	;   hp  atk  def  spd  spc
 
-	db WATER, ICE ; type
+	db FIRE, GROUND ; type
 	db 60 ; catch rate
 	db 203 ; base exp
 
 	INCBIN "gfx/pokemon/front/cloyster.pic", 0, 1 ; sprite dimensions
 	dw CloysterPicFront, CloysterPicBack
 
-	db WITHDRAW, SUPERSONIC, CLAMP, AURORA_BEAM ; level 1 learnset
+	db WITHDRAW, SAND_ATTACK, FIRE_SPIN, DIG ; level 1 learnset
 	db GROWTH_SLOW ; growth rate
 
 	; tm/hm learnset
-	tmhm TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  BUBBLEBEAM,   WATER_GUN,    \
-	     ICE_BEAM,     BLIZZARD,     HYPER_BEAM,   RAGE,         TELEPORT,     \
-	     MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         SELFDESTRUCT, \
-	     SWIFT,        REST,         EXPLOSION,    TRI_ATTACK,   SUBSTITUTE,   \
-	     SURF
+	tmhm TOXIC,        TAKE_DOWN,    HYPER_BEAM,   RAGE,         EARTHQUAKE,   \
+		 FISSURE,      DIG,          TELEPORT,     MIMIC,        DOUBLE_TEAM,  \
+		 REFLECT,      BIDE,         SELFDESTRUCT, FIRE_BLAST,   SWIFT,        \
+		 REST,         EXPLOSION,    TRI_ATTACK,   SUBSTITUTE
 	; end
 
 	db 0 ; padding
