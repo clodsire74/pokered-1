c039800f5ac750059020fd68f6a0f083b6c8bccd
clodsire74 <davidstormking0@gmail.com>
2025-10-15 14:14:31 -0700

    Update sfx.asm

--- data/moves/sfx.asm
+++ data/moves/sfx.asm
@@ -165,6 +165,10 @@ MoveSoundTable:
 	db SFX_BATTLE_26,          $f0, $ff ; SUPER_FANG
 	db SFX_NOT_VERY_EFFECTIVE, $01, $ff ; SLASH
 	db SFX_BATTLE_2C,          $d8, $04 ; SUBSTITUTE
+	db SFX_BATTLE_18,          $00, $80 ; BUG_BUZZ
+	db SFX_BATTLE_18,          $00, $80 ; SLUDGE_BOMB
+	db SFX_BATTLE_18,          $00, $80 ; SHADOW_BALL
+	db SFX_BATTLE_18,          $00, $80 ; POWDER_SNOW
 	db SFX_BATTLE_0B,          $00, $80 ; STRUGGLE
 	assert_table_length NUM_ATTACKS
 	db SFX_BATTLE_0B,          $00, $80
