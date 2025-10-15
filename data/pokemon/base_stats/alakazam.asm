abb8b81aabcf3346a50d79f32c8c98333f33b60a
clodsire74 <davidstormking0@gmail.com>
2025-10-15 12:50:59 -0700

    Update alakazam.asm

--- data/pokemon/base_stats/alakazam.asm
+++ data/pokemon/base_stats/alakazam.asm
@@ -3,7 +3,7 @@
 	db  55,  50,  45, 120, 135
 	;   hp  atk  def  spd  spc
 
-	db PSYCHIC_TYPE, PSYCHIC_TYPE ; type
+	db ICE, GROUND ; type
 	db 50 ; catch rate
 	db 186 ; base exp
 
@@ -15,11 +15,11 @@
 
 	; tm/hm learnset
 	tmhm MEGA_PUNCH,   MEGA_KICK,    TOXIC,        BODY_SLAM,    TAKE_DOWN,    \
-	     DOUBLE_EDGE,  HYPER_BEAM,   SUBMISSION,   COUNTER,      SEISMIC_TOSS, \
-	     RAGE,         DIG,          PSYCHIC_M,    TELEPORT,     MIMIC,        \
-	     DOUBLE_TEAM,  REFLECT,      BIDE,         METRONOME,    SKULL_BASH,   \
-	     REST,         THUNDER_WAVE, PSYWAVE,      TRI_ATTACK,   SUBSTITUTE,   \
-	     FLASH
+	     DOUBLE_EDGE,  ICE_BEAM,     BLIZZARD,     HYPER_BEAM,   SUBMISSION,   \
+	     COUNTER,      SEISMIC_TOSS, RAGE,         DIG,          PSYCHIC_M,    \
+         TELEPORT,     MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         \
+         METRONOME,    SKULL_BASH,   REST,         THUNDER_WAVE, PSYWAVE,      \
+         TRI_ATTACK,   SUBSTITUTE,   FLASH
 	; end
 
 	db 0 ; padding
