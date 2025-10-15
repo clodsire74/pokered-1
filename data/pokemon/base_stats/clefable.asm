7f62139634dcef47a72375f928c51a253f3c27f0
clodsire74 <davidstormking0@gmail.com>
2025-10-15 14:41:08 -0700

    Update clefable.asm

--- data/pokemon/base_stats/clefable.asm
+++ data/pokemon/base_stats/clefable.asm
@@ -3,14 +3,14 @@
 	db  95,  70,  73,  60,  85
 	;   hp  atk  def  spd  spc
 
-	db NORMAL, NORMAL ; type
+	db ROCK, FIGHTING ; type
 	db 25 ; catch rate
 	db 129 ; base exp
 
 	INCBIN "gfx/pokemon/front/clefable.pic", 0, 1 ; sprite dimensions
 	dw ClefablePicFront, ClefablePicBack
 
-	db SING, DOUBLESLAP, MINIMIZE, METRONOME ; level 1 learnset
+	db SING, ROCK_THROW, MINIMIZE, METRONOME ; level 1 learnset
 	db GROWTH_FAST ; growth rate
 
 	; tm/hm learnset
@@ -20,7 +20,7 @@
 	     SOLARBEAM,    THUNDERBOLT,  THUNDER,      PSYCHIC_M,    TELEPORT,     \
 	     MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         METRONOME,    \
 	     FIRE_BLAST,   SKULL_BASH,   REST,         THUNDER_WAVE, PSYWAVE,      \
-	     TRI_ATTACK,   SUBSTITUTE,   STRENGTH,     FLASH
+	     ROCK_SLIDE,   TRI_ATTACK,   SUBSTITUTE,   STRENGTH,     FLASH
 	; end
 
 	db 0 ; padding
