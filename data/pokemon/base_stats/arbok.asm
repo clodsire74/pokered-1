da7a1bf41e6285ac07d679f8e567ecc7240d86f3
clodsire74 <davidstormking0@gmail.com>
2025-10-15 12:52:08 -0700

    Update arbok.asm

--- data/pokemon/base_stats/arbok.asm
+++ data/pokemon/base_stats/arbok.asm
@@ -3,14 +3,14 @@
 	db  60,  85,  69,  80,  65
 	;   hp  atk  def  spd  spc
 
-	db POISON, POISON ; type
+	db NORMAL, ICE ; type
 	db 90 ; catch rate
 	db 147 ; base exp
 
 	INCBIN "gfx/pokemon/front/arbok.pic", 0, 1 ; sprite dimensions
 	dw ArbokPicFront, ArbokPicBack
 
-	db WRAP, LEER, POISON_STING, NO_MOVE ; level 1 learnset
+	db WRAP, LEER, BITE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
