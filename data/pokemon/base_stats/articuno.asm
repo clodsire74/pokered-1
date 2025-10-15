81552e0ef284391a349003b674356ee2051b8062
clodsire74 <davidstormking0@gmail.com>
2025-10-15 12:57:30 -0700

    Update articuno.asm

--- data/pokemon/base_stats/articuno.asm
+++ data/pokemon/base_stats/articuno.asm
@@ -3,21 +3,21 @@
 	db  90,  85, 100,  85, 125
 	;   hp  atk  def  spd  spc
 
-	db ICE, FLYING ; type
+	db DRAGON, GRASS ; type
 	db 3 ; catch rate
 	db 215 ; base exp
 
 	INCBIN "gfx/pokemon/front/articuno.pic", 0, 1 ; sprite dimensions
 	dw ArticunoPicFront, ArticunoPicBack
 
-	db PECK, ICE_BEAM, NO_MOVE, NO_MOVE ; level 1 learnset
+	db DRAGON_RAGE, RAZOR_LEAF, NO_MOVE, NO_MOVE ; level 1 learnset
 	db GROWTH_SLOW ; growth rate
 
 	; tm/hm learnset
 	tmhm RAZOR_WIND,   WHIRLWIND,    TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  \
-	     BUBBLEBEAM,   WATER_GUN,    ICE_BEAM,     BLIZZARD,     HYPER_BEAM,   \
-	     RAGE,         MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         \
-	     SWIFT,        SKY_ATTACK,   REST,         SUBSTITUTE,   FLY
+		 HYPER_BEAM,   RAGE,         MEGA_DRAIN,   SOLAR_BEAM,   DRAGON_RAGE,  \
+		 MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         SWIFT,        \
+		 SKY_ATTACK,   REST,         SUBSTITUTE,   FLY
 	; end
 
 	db 0 ; padding
